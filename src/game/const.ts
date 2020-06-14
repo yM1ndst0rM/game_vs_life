225be1e17e297530f1d54ecc5a4ce86014a1b103
YK <mountan_spirit-github@yahoo.com>
2020-06-14 15:40:06 +0200

    Add default starting configurations for both players

--- src/game/const.ts
+++ src/game/const.ts
@@ -1,3 +1,5 @@
+import { PutCellDiff } from "./diff";
+
 export enum CellType {
     OUT_OF_BOUNDS,
     OUT_OF_VISION,
@@ -22,3 +24,23 @@ export const Const = {
     PLAYER_KEY_LENGTH: 10,
     DEFAULT_TICK_DURATION_MS: 1000
 };
+
+export const STARTING_CONF_P1 =
+    new PutCellDiff( 4, 4, Const.PLAYER1_CELL_TYPE)
+    .plus(new PutCellDiff(4, 5, Const.PLAYER1_CELL_TYPE))
+    .plus(new PutCellDiff(5, 4, Const.PLAYER1_CELL_TYPE))
+    .plus(new PutCellDiff(5, 5, Const.PLAYER1_CELL_TYPE))
+    .plus(new PutCellDiff(8, 5, Const.PLAYER1_CELL_TYPE))
+    .plus(new PutCellDiff(9, 5, Const.PLAYER1_CELL_TYPE))
+    .plus(new PutCellDiff(10, 5, Const.PLAYER1_CELL_TYPE));
+
+const w = Const.MAP_DEFAULTS.width;
+const h = Const.MAP_DEFAULTS.height;
+export const STARTING_CONF_P2 =
+    new PutCellDiff(w - 4, h - 4, Const.PLAYER1_CELL_TYPE)
+        .plus(new PutCellDiff(w - 4, h - 5, Const.PLAYER1_CELL_TYPE))
+        .plus(new PutCellDiff(w - 5, h - 4, Const.PLAYER1_CELL_TYPE))
+        .plus(new PutCellDiff(w - 5, h - 5, Const.PLAYER1_CELL_TYPE))
+        .plus(new PutCellDiff(w - 8, h - 5, Const.PLAYER1_CELL_TYPE))
+        .plus(new PutCellDiff(w - 9, h - 5, Const.PLAYER1_CELL_TYPE))
+        .plus(new PutCellDiff(w - 10, h - 5, Const.PLAYER1_CELL_TYPE));
